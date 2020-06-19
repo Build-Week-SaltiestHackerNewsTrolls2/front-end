4ea8c89f0469ff297d41cf7f4707cb9201cac494
bradpittstwin <47493177+bradpittstwin@users.noreply.github.com>
2020-06-18 21:03:31 -0600

    hopefully fixed

---
+++ saltiest_hackers_news_trolls/src/components/Form.js
@@ -0,0 +1,95 @@
+import React, {useState} from 'react'
+import { Card, Form, FormGroup, Input, Button, Dropdown, DropdownToggle} from 'reactstrap'
+import axios from 'axios'
+import * as yup from 'yup'
+
+const Form = () => {
+    const [formData, setformData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        country: ''
+    });
+    const [country, setCountry] = useState([]);
+    useEffect(() => {
+        axios
+            .get(`https://restcountries.eu/rest/v2/all`)
+            .then(response => {
+                setCountry(response.data);
+            })
+            .catch(error => {
+                console.log('No go', error);
+            });
+    }, []);
+
+    const validatation = yup.object().shape({
+        firstName: yup.string().required(),
+        lastName: yup.string().required(),
+        email: yup.required().string(),
+        password: yup.required().string()
+    })
+
+    const submit = () => {
+        validatation.validate(formData).then(() => {
+            axios.post('', formData).then((res) => {
+                console.log("This is your data", res.data)
+            })
+        })
+    }
+
+    const handleChange = (e) => {
+        setformData({...formData, [e.target.name]: e.target.value})
+    }
+    return(
+        <>
+        <Card style={{ color: red }}>
+            <h2 style={{color: 'white', margin: '0 auto'}}>
+                Enter Your Information
+            </h2>
+        </Card>
+
+        <Form style={{margin: '5%'}} onSubmit={(e) => {
+            e.preventDefault()
+            submit()
+        }}>
+
+            <FormGroup>
+                <legend>First Name</legend>
+                <Input type='text' name='firstName' placeholder='Please enter your first name here' value={formData.firstName} onChange={handleChange}/>
+            </FormGroup>
+
+            <FormGroup>
+                <legend>Last Name</legend>
+                <Input type='text' name='lastName' placeholder='Please enter your last name here' value={formData.lastName} onChange={handleChange}/>
+            </FormGroup>
+
+                     <FormGroup>
+                        <legend>Email</legend>
+                        <Input type='email' name='email' placeholder='Please enter a vaild email' value={formData.email} onChange={handleChange}/>
+                    </FormGroup>
+
+                    <FormGroup>
+                        <legend>Password</legend>
+                        <Input type='password' name='password' value='password' onChange={handleChange}/>
+                    </FormGroup>
+
+                    <FormGroup>
+                <Dropdown isOpen={dropdownOpen} toggle={toggle} value={formData.country} onChange={handleChange} >
+                    <DropdownToggle caret>
+                    {country.map(country => {
+                        return <option>{country.name}</option>;
+                        })}
+                    </DropdownToggle>
+                </Dropdown>
+            </FormGroup>
+
+            <Button>
+                Submit
+            </Button>
+        </Form>
+        </>
+    )
+}
+
+export default Form;
