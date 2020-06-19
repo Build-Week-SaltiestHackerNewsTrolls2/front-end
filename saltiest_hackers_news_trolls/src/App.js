8f898601a63392b02702c7d3832b16ed8a4d3c40
Adam-Young <awy6448@gmail.com>
2020-06-18 23:37:13 -0400

    fixed up form

--- saltiest_hackers_news_trolls/src/App.js
+++ saltiest_hackers_news_trolls/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import Login from "./components/Login"
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import REGForm from "./components/REGform"
 
 function App() {
   return (
     <div className="App">
       <Router>
       <Switch>
-        <Route exact path="/" component={Login} />
+        <Route path="/" component={Login} />
+        <Route path="/reg" component={REGForm} />
       </Switch>
       </Router>
     </div>
